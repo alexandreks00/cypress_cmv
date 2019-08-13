0315d739bbbdfa1832494819526543aae3c6c33f
Alexandre Kenji Shimizu <kenji@Mac-mini-de-Alexandre.local>
2019-08-13 13:55:44 -0300

    clicking issues still ocurring

--- cypress/integration/examples/customer.js
+++ cypress/integration/examples/customer.js
@@ -12,11 +12,17 @@ context('Basic tests', () => {
         // cy.get('button').contains("Cotações").click()
         // cy.get('button').contains("Pedidos").click()
         // cy.get('button').contains("Alertas").click()
-        cy.get('button').contains("compras").click()
+        //cy.get('button').contains("compras").click()
         cy.get('a[href="/shopping-list"]').click()
-        cy.get('a[href="/shopping-list/6/maintenance').click()
-        cy.get('button').contains("Confirmar").click()
-        cy.get('button').contains("OK").click()
+        //cy.get('a[href="/shopping-list/6/maintenance').click()
+        // cy.get('button').contains("Confirmar").click()
+        // cy.get('button').contains("OK").click()
+        //cy.get('button').contains("preencher").first().click()
+        cy.visit('https://customer.dev.cmvtech.com.br/shopping-list/6')
+        cy.get('*[alt="check-icon"]').click()
+        cy.get('button').contains("confirmar lista de compras").click()
+        cy.get('button').contains("enviar cotação").click()
+  
         
         
 
